0cefbf876ce0c9db0fe772c1394ac389d4823c09
Eli <berraeliel@gmail.com>
2026-02-10 22:07:34 -0300

    feat: body length dynamically set

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ import socket
 host = "localhost"
 port = 8000
 
-custom_request = (
-"""POST /api/users HTTP/1.1
-Host: example.com
-Content-Type: application/json
-Content-Length: 55
-
-{
+body = """{
   "name": "Jane Doe",
   "email": "jane@example.com"
-}
-"""
-)
+}"""
+
+content_length = len(body.encode('utf-8'))
+
+custom_request = f"""POST /api/users HTTP/1.1\r
+Host: example.com\r
+Content-Type: application/json\r
+Content-Length: {content_length}\r
+\r
+{body}"""
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
-    s.sendall(custom_request.encode())
+    s.sendall(custom_request.encode('utf-8'))
     response = s.recv(4096)
     print(response.decode())
